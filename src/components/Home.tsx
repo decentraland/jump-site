4a79dd9100c07c47474f3d3b31eac3a610d90a60
Gonzalo DCL <gonzalo@decentraland.org>
2024-07-23 11:24:20 -0300

    init commit

---
+++ src/components/Home.tsx
@@ -0,0 +1,76 @@
+import React from 'react'
+import { useParams, useSearchParams } from 'react-router-dom'
+import { Box, Link, Typography } from 'decentraland-ui2'
+
+import './Home.css'
+import Card from './Card'
+import { Metadata, getLatestRelease, isEns, queryData } from '../utils'
+
+
+
+function Home() {
+  const [searchParams] = useSearchParams();
+  const position = searchParams.get('position') ?? '0,0'
+  const realm = searchParams.get('realm') ?? 'main'
+  const [metadata, setMetadata] = React.useState<Metadata | undefined>()
+  const [link, setLink] = React.useState<string>()
+  const [downloadOption, setShowDownloadOption] = React.useState<boolean>(false)
+
+  React.useEffect(() => {
+    const fetchMetadata = async () => {
+      try {
+        setMetadata(await queryData(realm, position))
+      } catch (error) {
+        console.error('Error fetching metadata:', error)
+      }
+    }
+    fetchMetadata()
+  }, [position, realm])
+
+  React.useEffect(() => {
+    async function getLatest() {
+      const latestRelease = await getLatestRelease()
+      setLink(latestRelease.browser_download_url)
+    }
+    getLatest()
+  }, [])
+
+
+  const handleOpenApp = () => {
+    const appUrl = `decentraland://`;
+
+    // Attempt to open the Decentraland app
+    window.location.href = appUrl;
+
+    // Set a timeout to check if the app opened
+    setTimeout(() => {
+      // If the page is still here after a short delay, show download option
+      setShowDownloadOption(true);
+    }, 1500);
+  };
+
+  const title = (realm && isEns(realm)) ? `World: ${realm}` : `Genesis City at ${position}`
+
+  return (
+    <Box className="explorer-website-start">
+      <Typography variant='h3' sx={{ padding: 10, marginTop: -30, textAlign: 'center'}}>
+        {title}
+      </Typography>
+      <Card
+        imageUrl={metadata?.image ?? ''}
+        title={metadata?.title ?? "Unkown title"}
+        subtitle={metadata?.description ?? "Unkown description"}
+        buttonText="Jump in"
+        onButtonClick={handleOpenApp}
+      />
+      <Link target="_blank" rel="noopener noreferrer"  variant="h5" href={link} sx={{ color: 'white', mt: 5, visibility: downloadOption ? 'visible' : 'hidden' }}>
+        Download Desktop Explorer
+      </Link>
+    </Box>
+  )
+}
+
+export default Home
+
+
+
---
+++ src/utils.ts
@@ -0,0 +1,63 @@
+/**
+ * KERNEL CODE :)
+ * Thanks kernel
+ */
+
+export type Metadata = {
+  description: string
+  title: string
+  image: string
+}
+
+export function isEns(str: string | undefined): str is `${string}.eth` {
+  return !!str?.match(/^[a-zA-Z0-9.]+\.eth$/)?.length
+}
+
+export async function queryData(realm: string, position: string): Promise<Metadata | undefined> {
+  const url = isEns(realm) ? `https://places.decentraland.org/api/worlds?names=${realm.toLowerCase()}` : `https://places.decentraland.org/api/places?positions=${position}`
+  const resp = await fetch(url)
+  const data: { data: Metadata[] } = await resp.json()
+  return data.data[0]
+}
+
+export interface GithubReleaseResponse {
+  browser_download_url: string;
+  version: string;
+}
+
+export let latestRelease: GithubReleaseResponse
+
+export async function getLatestRelease(): Promise<GithubReleaseResponse> {
+  if (latestRelease) return latestRelease
+  const resp = await fetch(`https://api.github.com/repos/decentraland/unity-explorer/releases/latest`);
+  if (resp.status === 200) {
+    const data = (await resp.json()) as { assets: Record<string, string>[]; name: string };
+    const os = getOSName();
+    const asset = data.assets.find((asset: Record<string, string>) => asset.name.includes(os.toLowerCase()));
+    if (asset) {
+      return latestRelease = {
+        browser_download_url: asset.browser_download_url,
+        version: data.name,
+      }
+    } else {
+      throw new Error('No asset found for your platform');
+    }
+  }
+
+  throw new Error('Failed to fetch latest release: ' + JSON.stringify(resp));
+}
+getLatestRelease()
+
+type OSName = 'windows' | 'macos' | 'Unknown'
+
+function getOSName(): OSName {
+  const userAgent = window.navigator.userAgent.toLowerCase();
+
+  if (userAgent.indexOf('win') > -1) {
+    return 'windows'
+  } else if (userAgent.indexOf('mac') > -1) {
+    return 'macos'
+  }
+
+  return 'Unknown'
+}
