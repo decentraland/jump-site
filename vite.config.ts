7d49469c8f1ad720f0a9b4a45909ac07e54e510b
Gabriel Diaz <gabrieldiaz31@gmail.com>
2025-03-21 17:37:09 -0300

    chore: Add prebuild script

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => {
+export default defineConfig(({ command, mode }) => {
   const envVariables = loadEnv(mode, process.cwd());
 
   return {
@@ -11,5 +11,6 @@ export default defineConfig(({ mode }) => {
         VITE_REACT_APP_DCL_DEFAULT_ENV: envVariables.VITE_REACT_APP_DCL_DEFAULT_ENV,
       },
     },
+    ...(command === 'build' ? { base: envVariables.VITE_BASE_URL } : undefined),
   };
 });
