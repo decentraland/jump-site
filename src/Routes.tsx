4d2b9648cb0b7ce7ab88754dde78c94c303ac42b
Gabriel Diaz <gabrieldiaz31@gmail.com>
2025-03-21 19:40:08 -0300

    fix: Add default route

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,14 +1,15 @@
-import { type FC } from 'react'
+import { memo, type FC } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import { MainPage } from './components/Pages/MainPage'
 import { usePageTracking } from './hooks/usePageTracking'
 
-export const AppRoutes: FC = () => {
+export const AppRoutes: FC = memo(() => {
   usePageTracking()
 
   return (
     <Routes>
       <Route path="/" element={<MainPage />} />
+      <Route path="*" element={<MainPage />} />
     </Routes>
   )
-}
+})
