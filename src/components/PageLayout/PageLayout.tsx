73ebeedf6b85d01cb955988aca1d12cbc00b7d11
Gabriel Diaz <gabrieldiaz31@gmail.com>
2025-03-21 19:39:56 -0300

    fix: Remove unused files

--- src/components/PageLayout/PageLayout.tsx
+++
@@ -1,9 +0,0 @@
-import { type FC } from 'react'
-import classNames from 'classnames'
-import { Box } from 'decentraland-ui2'
-import { Props } from './PageLayout.types'
-import styles from './PageLayout.module.css'
-
-export const PageLayout: FC<Props> = ({ children, className }: Props) => {
-  return <Box className={classNames(styles.page, className)}>{children}</Box>
-}
--- src/components/PageLayout/PageLayout.types.ts
+++
@@ -1,5 +0,0 @@
-import type { PropsWithChildren } from 'react'
-
-export type Props = {
-  className?: string
-} & PropsWithChildren
