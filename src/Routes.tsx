75f277f6efee537c824a50fe8c5145902c812802
Gabriel Diaz <gabrieldiaz31@gmail.com>
2025-03-21 16:57:13 -0300

    refactor: Create Routes component to handle all app routes

---
+++ src/Routes.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { MainPage } from './components/Pages/MainPage';
+import { usePageTracking } from './hooks/usePageTracking';
+
+export const AppRoutes: React.FC = () => {
+  usePageTracking();
+
+  return (
+    <Routes>
+      <Route path="/" element={<MainPage />} />
+    </Routes>
+  );
+};
