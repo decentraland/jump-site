03f9fdd1fe854ca7c38654954612c0afb9a347cd
Gabriel Diaz <gabrieldiaz31@gmail.com>
2025-03-21 16:59:00 -0300

    feat: Add providers to main file

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from './App';
-import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import { dark, ThemeProvider } from 'decentraland-ui2/dist/theme';
+import { AnalyticsProvider } from './contexts/analytics/AnalyticsProvider';
+import { config } from './config';
+import { AppRoutes } from './Routes';
 
+const basename = /^decentraland.(zone|org|today)$/.test(window.location.host) ? '/jump' : '/';
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename={basename}>
+      <ThemeProvider theme={dark}>
+        <AnalyticsProvider writeKey={config.get('SEGMENT_API_KEY')}>
+          <AppRoutes />
+        </AnalyticsProvider>
+      </ThemeProvider>
+    </BrowserRouter>
   </React.StrictMode>,
 );
