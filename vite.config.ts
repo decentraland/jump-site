680ac58fcf7cb5efdb8344f7e2c4c8d4b1cc8a36
Gabriel Diaz <gabrieldiaz31@gmail.com>
2025-03-24 12:46:36 -0300

    fix: eslint

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,18 @@
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite'
 
 export default defineConfig(({ command, mode }) => {
-  const envVariables = loadEnv(mode, process.cwd());
+  const envVariables = loadEnv(mode, process.cwd())
 
   return {
     plugins: [react()],
     define: {
+      // eslint-disable-next-line @typescript-eslint/naming-convention
       'process.env': {
-        VITE_REACT_APP_DCL_DEFAULT_ENV: envVariables.VITE_REACT_APP_DCL_DEFAULT_ENV,
-      },
+        // eslint-disable-next-line @typescript-eslint/naming-convention
+        VITE_REACT_APP_DCL_DEFAULT_ENV: envVariables.VITE_REACT_APP_DCL_DEFAULT_ENV
+      }
     },
-    ...(command === 'build' ? { base: envVariables.VITE_BASE_URL } : undefined),
-  };
-});
+    ...(command === 'build' ? { base: envVariables.VITE_BASE_URL } : undefined)
+  }
+})
