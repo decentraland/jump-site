ea052070dbe6bc5c73bab5340b61a3a523aaff95
Gabriel Diaz <gabrieldiaz31@gmail.com>
2025-04-02 15:10:55 -0300

    fix: lint

--- src/components/Card/Card.styled.ts
+++ src/components/Card/Card.styled.ts
@@ -1,4 +1,5 @@
 import styled from '@emotion/styled'
+import { type Theme } from '@mui/material'
 
 export const ImageContainer = styled('div')({
   width: '100%',
@@ -16,67 +17,79 @@ export const Image = styled('img')({
   transition: 'transform 0.3s ease'
 })
 
-export const CardContainer = styled('div')(({ theme }) => ({
-  position: 'relative',
-  height: '400px',
-  width: '600px',
-  overflow: 'hidden',
-  borderRadius: '16px',
-  boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
-  ['&:hover img']: {
-    transform: 'scale(1.1)'
-  },
-  [theme.breakpoints.down('xs')]: {
-    width: 'calc(100vw - 32px)',
-    height: '250px',
-    margin: '0 16px'
-  },
-  [theme.breakpoints.between('xs', 'sm')]: {
-    width: 'calc(100vw - 32px)',
-    height: '300px',
-    margin: '0 16px'
+export const CardContainer = styled('div')(props => {
+  const theme = props.theme as Theme
+  return {
+    position: 'relative',
+    height: '400px',
+    width: '600px',
+    overflow: 'hidden',
+    borderRadius: '16px',
+    boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+    ['&:hover img']: {
+      transform: 'scale(1.1)'
+    },
+    [theme.breakpoints.down('xs')]: {
+      width: 'calc(100vw - 32px)',
+      height: '250px',
+      margin: '0 16px'
+    },
+    [theme.breakpoints.between('xs', 'sm')]: {
+      width: 'calc(100vw - 32px)',
+      height: '300px',
+      margin: '0 16px'
+    }
   }
-}))
+})
 
-export const Content = styled('div')(({ theme }) => ({
-  display: 'flex',
-  position: 'absolute',
-  alignItems: 'center',
-  bottom: 0,
-  left: 0,
-  right: 0,
-  padding: '20px',
-  background: 'rgba(0, 0, 0, 0.6)',
-  color: 'white',
-  [theme.breakpoints.down('xs')]: {
-    padding: '16px'
+export const Content = styled('div')(props => {
+  const theme = props.theme as Theme
+  return {
+    display: 'flex',
+    position: 'absolute',
+    alignItems: 'center',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    padding: '20px',
+    background: 'rgba(0, 0, 0, 0.6)',
+    color: 'white',
+    [theme.breakpoints.down('xs')]: {
+      padding: '16px'
+    }
   }
-}))
+})
 
 export const Text = styled('div')({
   flexGrow: 1
 })
 
-export const Title = styled('h2')(({ theme }) => ({
-  fontSize: '24px',
-  fontWeight: 700,
-  margin: '0 0 10px',
-  color: 'white',
-  [theme.breakpoints.down('xs')]: {
-    fontSize: '20px',
-    margin: '0 0 6px'
+export const Title = styled('h2')(props => {
+  const theme = props.theme as Theme
+  return {
+    fontSize: '24px',
+    fontWeight: 700,
+    margin: '0 0 10px',
+    color: 'white',
+    [theme.breakpoints.down('xs')]: {
+      fontSize: '20px',
+      margin: '0 0 6px'
+    }
   }
-}))
+})
 
-export const Subtitle = styled('p')(({ theme }) => ({
-  margin: '4px 0 0',
-  fontSize: '0.9em',
-  opacity: 1,
-  [theme.breakpoints.down('xs')]: {
-    fontSize: '0.8em',
-    margin: '2px 0 0'
+export const Subtitle = styled('p')(props => {
+  const theme = props.theme as Theme
+  return {
+    margin: '4px 0 0',
+    fontSize: '0.9em',
+    opacity: 1,
+    [theme.breakpoints.down('xs')]: {
+      fontSize: '0.8em',
+      margin: '2px 0 0'
+    }
   }
-}))
+})
 
 export const LoadingContainer = styled('div')({
   display: 'flex',
