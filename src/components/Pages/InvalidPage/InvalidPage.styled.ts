f1cae3838b718fe8c4b4b6a8684cb25dbd497a86
Gabriel Diaz <gabrieldiaz31@gmail.com>
2025-05-27 17:59:12 -0300

    feat: Add Responsive design to the InvalidPage

--- src/components/Pages/InvalidPage/InvalidPage.styled.ts
+++ src/components/Pages/InvalidPage/InvalidPage.styled.ts
@@ -1,16 +1,55 @@
 import styled from '@emotion/styled'
+import { Box } from 'decentraland-ui2'
 import backgroundInvalidEventImage from '../../../assets/background-invalid-events-page.png'
 import backgroundInvalidPlaceImage from '../../../assets/background-invalid-places-page.png'
 import { MainPageContainer } from '../../MainPageContainer/MainPage.styled'
 
-export const InvalidPageContainer = styled(MainPageContainer)<{ isEventPage: boolean }>(({ isEventPage }) => {
+export const InvalidPageContainer = styled(MainPageContainer)<{ isEventPage: boolean; isMobile: boolean }>(({ isEventPage, isMobile }) => {
   const backgroundImage = isEventPage ? backgroundInvalidEventImage : backgroundInvalidPlaceImage
+  const linearGradient = isMobile
+    ? 'linear-gradient(to bottom, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0))'
+    : 'linear-gradient(104.62deg, #7106A0 17.97%, rgba(113, 6, 160, 0) 75.95%)'
   return {
     justifyContent: 'center',
     position: 'relative',
     overflow: 'hidden',
-    backgroundImage: `linear-gradient(104.62deg, #7106A0 17.97%, rgba(113, 6, 160, 0) 75.95%), url(${backgroundImage})`,
+    backgroundImage: `${linearGradient}, url(${backgroundImage})`,
     backgroundSize: 'cover',
-    backgroundPosition: 'center'
+    backgroundPosition: 'center',
+    '@media (max-width: 600px)': {
+      justifyContent: 'flex-start',
+      padding: '50px 18px 0 18px'
+    }
+  }
+})
+
+export const ContentBox = styled(Box)({
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  borderRadius: '24px',
+  padding: '48px',
+  backgroundColor: '#36044CF2',
+  width: '691px',
+  '@media (max-width: 600px)': {
+    width: '100%',
+    padding: '48px 24px',
+    alignSelf: 'center'
+  }
+})
+
+export const MobileActionsContainer = styled('div')({
+  position: 'fixed',
+  bottom: 0,
+  left: 0,
+  right: 0,
+  padding: 24,
+  background: 'linear-gradient(100.12deg, #130119 0%, #320524 100%)',
+  borderTop: '1px solid rgba(255, 255, 255, 0.1)',
+  display: 'none',
+  '@media (max-width: 600px)': {
+    display: 'flex',
+    flexDirection: 'column',
+    gap: '16px'
   }
 })
--- src/components/Pages/InvalidPage/InvalidPage.tsx
+++ src/components/Pages/InvalidPage/InvalidPage.tsx
@@ -1,34 +1,30 @@
 import { FC, memo } from 'react'
-import { Box, Button, Typography } from 'decentraland-ui2'
+import HomeIcon from '@mui/icons-material/Home'
+import { Button, Typography, useMobileMediaQuery } from 'decentraland-ui2'
 import { config } from '../../../config'
 import { useFormatMessage } from '../../../hooks/useFormatMessage'
 import { JumpInButton } from '../../JumpInButton'
 import { LinkButton } from '../../LinkButton'
-import { InvalidPageContainer } from './InvalidPage.styled'
+import { InvalidPageContainer, ContentBox, MobileActionsContainer } from './InvalidPage.styled'
 
 export const InvalidPage: FC<{ isEventPage: boolean }> = memo(({ isEventPage }) => {
   const formatMessage = useFormatMessage()
+  const isMobile = useMobileMediaQuery()
 
   return (
-    <InvalidPageContainer isEventPage={isEventPage}>
-      <Box
-        display="flex"
-        flexDirection="column"
-        justifyContent="center"
-        borderRadius="24px"
-        padding="48px"
-        bgcolor="#36044CF2"
-        width="691px"
-      >
+    <InvalidPageContainer isEventPage={isEventPage} isMobile={isMobile}>
+      <ContentBox>
         <Typography variant="h3" mb="24px">
           {formatMessage('invalid_page.title')}
         </Typography>
         <Typography variant="h6" mb="48px">
           {formatMessage('invalid_page.message')}
         </Typography>
-        <JumpInButton position="0,0" size="large" fullWidth sx={{ marginBottom: '16px' }}>
-          {formatMessage('invalid_page.jump_in_button')}
-        </JumpInButton>
+        {!isMobile ? (
+          <JumpInButton position="0,0" size="large" fullWidth sx={{ marginBottom: '16px' }}>
+            {formatMessage('invalid_page.jump_in_button')}
+          </JumpInButton>
+        ) : null}
         {isEventPage ? (
           <Button
             variant="contained"
@@ -41,16 +37,23 @@ export const InvalidPage: FC<{ isEventPage: boolean }> = memo(({ isEventPage }) 
             {formatMessage('invalid_page.explore_events')}
           </Button>
         ) : null}
-        <Typography variant="subtitle1" color="white" textAlign="center">
+        <Typography variant={isMobile ? 'h6' : 'subtitle1'} color="white" textAlign="center">
           {formatMessage('invalid_page.learn_more', {
             a: (text: string) => (
               <LinkButton variant="text" color="primary" href={config.get('HOME_URL')} target="_blank" rel="noopener noreferrer">
-                {text}
+                <Typography variant={isMobile ? 'h6' : 'subtitle1'}>{text}</Typography>
               </LinkButton>
             )
           })}
         </Typography>
-      </Box>
+      </ContentBox>
+      {isMobile && (
+        <MobileActionsContainer>
+          <Button variant="contained" color="primary" href={config.get('HOME_URL')} size="large" fullWidth startIcon={<HomeIcon />}>
+            {formatMessage('invalid_page.go_home_button')}
+          </Button>
+        </MobileActionsContainer>
+      )}
     </InvalidPageContainer>
   )
 })
