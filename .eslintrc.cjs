b4ee45802d45b1b0da2bfe4fd05a42ce1fcabd13
Gabriel Diaz <gabrieldiaz31@gmail.com>
2025-05-27 15:06:19 -0300

    chore: Fix import eslint naming convention

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,5 +3,15 @@ module.exports = {
   extends: ['@dcl/eslint-config/dapps'],
   parserOptions: {
     project: ['./tsconfig.json', './tsconfig.node.json', './tsconfig.app.json']
+  },
+  rules: {
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        selector: 'import',
+        format: ['camelCase', 'PascalCase']
+      }
+    ]
   }
 }
