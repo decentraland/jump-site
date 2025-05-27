d86a720956ad2061f136d0fece3acf20a7981632
Gabriel Diaz <gabrieldiaz31@gmail.com>
2025-05-27 17:22:22 -0300

    feat: Add LinkButton

---
+++ src/components/LinkButton/LinkButton.styled.ts
@@ -0,0 +1,17 @@
+import styled from '@emotion/styled'
+import { Button } from 'decentraland-ui2'
+
+export const StyledLinkButton = styled(Button)({
+  '&:hover': {
+    backgroundColor: 'transparent',
+    textDecoration: 'none'
+  },
+  '&:focus': {
+    backgroundColor: 'transparent',
+    textDecoration: 'none'
+  },
+  textTransform: 'none',
+  padding: 0,
+  minWidth: 'auto',
+  fontWeight: 'inherit'
+})
