217ece6de883e25fb54108dbf054ca9aed467283
Gabriel Diaz <gabrieldiaz31@gmail.com>
2025-05-27 14:42:26 -0300

    feat: Container to show MobileCard or DesktopCard

---
+++ src/components/ResponsiveCard/ResponsiveCard.tsx
@@ -0,0 +1,31 @@
+import { memo, type FC, type ReactNode } from 'react'
+import { useMobileMediaQuery } from 'decentraland-ui2'
+import { type CardData } from '../../utils/cardDataTransformers'
+import { type Creator } from '../../utils/peerApi'
+import { Card } from '../Card'
+import { MobileCard } from '../MobileCard'
+
+type ResponsiveCardProps = {
+  data: CardData
+  isLoading?: boolean
+  children?: ReactNode
+  creator?: Creator
+}
+
+export const ResponsiveCard: FC<ResponsiveCardProps> = memo(({ data, isLoading = false, children, creator }) => {
+  const isMobile = useMobileMediaQuery()
+
+  if (isMobile) {
+    return (
+      <MobileCard data={data} isLoading={isLoading} creator={creator}>
+        {children}
+      </MobileCard>
+    )
+  }
+
+  return (
+    <Card data={data} isLoading={isLoading} creator={creator}>
+      {children}
+    </Card>
+  )
+})
