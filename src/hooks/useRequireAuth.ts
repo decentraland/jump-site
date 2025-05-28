a41b1d70ea928920062d9b39ce3c3cdebc3b821d
Gabriel Diaz <gabrieldiaz31@gmail.com>
2025-05-28 10:48:56 -0300

    fix: Redirection to auth

--- src/hooks/useRequireAuth.ts
+++ src/hooks/useRequireAuth.ts
@@ -1,15 +1,12 @@
-import { useLocation } from 'react-router-dom'
 import { config } from '../config'
 import { useAuth } from '../contexts/auth'
 
 export function useRequireAuth<T extends (...args: unknown[]) => unknown>(callback: T): T {
   const { isSignedIn } = useAuth()
-  const location = useLocation()
-  console.log(`${location.pathname}${location.search}`)
+
   return ((...args: unknown[]) => {
     if (!isSignedIn) {
-      const currentPath = `${location.pathname}${location.search}`
-      window.location.href = `${config.get('AUTH_URL')}/?redirectTo=${encodeURIComponent(currentPath)}`
+      window.location.href = `${config.get('AUTH_URL')}/login?redirectTo=${encodeURIComponent(window.location.href)}`
       return
     }
     return callback(...args)
